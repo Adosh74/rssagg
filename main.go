dfcf930b0392f06ad7d58a92dd3afdebf4400683
Mohamed Shebl <hamdysheblm@gmail.com>
2024-12-16 22:02:13 +0200

    Add: database connection, new routes under /v1 and add create user route

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/Adosh74/rssagg/internal/database"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	fmt.Println("Rss Feed Aggregator Server...")
@@ -23,6 +30,22 @@ func main() {
 		log.Fatal("Port not found in the environment")
 	}
 
+	dbUrl := os.Getenv("DB_URL")
+
+	if dbUrl == "" {
+		log.Fatal("Port not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres", dbUrl)
+
+	if err != nil {
+		log.Fatal("Error connecting db", err)
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
+	}
+
 	router := chi.NewRouter()
 
 	router.Use(cors.Handler(cors.Options{
@@ -33,8 +56,15 @@ func main() {
 		AllowCredentials: false,
 		MaxAge:           300,
 	}))
-
 	router.Use(middleware.Logger)
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerUser)
+
+	router.Mount("/v1", v1Router)
 
 	log.Printf("Starting server on port %v", portString)
 	srv := &http.Server{
@@ -42,7 +72,7 @@ func main() {
 		Addr:    ":" + portString,
 	}
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 
 	if err != nil {
 		log.Fatal(err)
