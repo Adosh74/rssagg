60bc2101a013c3e8dc1ea342d78937720ac1f9ab
Mohamed Shebl <hamdysheblm@gmail.com>
2024-12-17 18:37:22 +0200

    Add: /users with method GET and /feeds with method POST

--- main.go
+++ main.go
@@ -63,6 +63,8 @@ func main() {
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
 	v1Router.Post("/users", apiCfg.handlerUser)
+	v1Router.Get("/users", apiCfg.middlewareAuth(handlerGetUser))
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
 
 	router.Mount("/v1", v1Router)
 
