3bf1f4c18e88ce5433d863f625c347fc2770c1dc
Mohamed Shebl <hamdysheblm@gmail.com>
2024-12-17 18:34:19 +0200

    Add: feed struct and databaseFeedToFeed function

--- models.go
+++ models.go
@@ -14,11 +14,31 @@ type User struct {
 	Name      string    `json:"name"`
 }
 
-func databaseUserToUser(dbUser database.CreateUserParams) User {
+type Feed struct {
+	ID        uuid.UUID     `json:"id"`
+	CreatedAt time.Time     `json:"created_at"`
+	UpdatedAt time.Time     `json:"updated_at"`
+	Name      string        `json:"name"`
+	Url       string        `json:"url"`
+	UserID    uuid.NullUUID `json:"user_id"`
+}
+
+func databaseUserToUser(dbUser database.User) User {
 	return User{
 		ID:        dbUser.ID,
 		CreatedAt: dbUser.CreatedAt,
 		UpdatedAt: dbUser.UpdatedAt,
 		Name:      dbUser.Name,
+	}
+}
+
+func databaseFeedToFeed(dbFeed database.Feed) Feed {
+	return Feed{
+		ID:        dbFeed.ID,
+		CreatedAt: dbFeed.CreatedAt,
+		UpdatedAt: dbFeed.UpdatedAt,
+		Name:      dbFeed.Name,
+		Url:       dbFeed.Url,
+		UserID:    dbFeed.UserID,
 	}
 }
