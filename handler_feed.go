e9bca02df2f19f94f28b1d2776a8baf541083952
Mohamed Shebl <hamdysheblm@gmail.com>
2024-12-17 18:36:00 +0200

    Add: handler create feed with using user coming from middle-ware auth

---
+++ handler_feed.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/Adosh74/rssagg/internal/database"
+	"github.com/google/uuid"
+)
+
+func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
+	type parameters struct {
+		Name string `json:"name"`
+		Url  string `json:"url"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		responseWithError(w, 400, fmt.Sprintf("Error parsing JSON: %s", err))
+		return
+	}
+	modifiedUserId := uuid.NullUUID{
+		UUID:  user.ID,
+		Valid: true,
+	}
+
+	feed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+		Url:       params.Url,
+		UserID:    modifiedUserId,
+	})
+
+	if err != nil {
+		responseWithError(w, 400, fmt.Sprintf("Error creating feed: %v", err))
+	}
+
+	responseWithJson(w, 201, databaseFeedToFeed(feed))
+}
