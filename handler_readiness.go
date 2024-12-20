2d68fca15a543fdd9e4bad986da09b681cd89734
Mohamed Shebl <hamdysheblm@gmail.com>
2024-12-20 18:02:11 +0200

    Update: send message with app name on /healthz route

--- handler_readiness.go
+++ handler_readiness.go
@@ -1,10 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	responseWithJson(w, 200, struct{}{
-   status: "success"
-})
+	appName := os.Getenv("APP_NAME")
+	port := os.Getenv("PORT")
+
+	message := appName + " is ready" + " on port " + port
+	responseWithJson(w, 200, struct {
+		Message string `json:"msg"`
+	}{
+		Message: message,
+	})
 
 }
