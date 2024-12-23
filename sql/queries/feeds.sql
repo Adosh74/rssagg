5d8c3e5d5c0e022312a605b8c4a20937b78a3b44
Mohamed Shebl <hamdysheblm@gmail.com>
2024-12-23 15:33:00 +0200

    Add: MarkedFeedFetched query

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -10,3 +10,9 @@ SELECT *
 FROM feeds
 ORDER BY last_fetched_at ASC NULLS FIRST
 LIMIT 1;
+-- name: MarkedFeedFetched :one
+UPDATE feeds
+SET last_fetched_at = Now(),
+    updated_at = Now()
+WHERE id = $1
+RETURNING *;
