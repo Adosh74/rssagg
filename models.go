47028136e2f0debb47a6bb797c7ffb9250aef23c
Mohamed Shebl <hamdysheblm@gmail.com>
2024-12-24 21:20:14 +0200

    Update: handle if description null

--- models.go
+++ models.go
@@ -37,7 +37,7 @@ type Post struct {
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 	Title       string    `json:"title"`
-	Description string    `json:"description"`
+	Description *string   `json:"description"`
 	PublishedAt time.Time `json:"publish_at"`
 	Url         string    `json:"url"`
 	FeedID      uuid.UUID `json:"feed_id"`
@@ -95,12 +95,19 @@ func databaseFeedFollowToFeedFollows(dbFeedFollow []database.FeedFollow) []FeedF
 }
 
 func databasePostToPost(dbPost database.Post) Post {
+
+	var description *string
+
+	if dbPost.Description.Valid {
+		description = &dbPost.Description.String
+	}
+
 	return Post{
 		ID:          dbPost.ID,
 		CreatedAt:   dbPost.CreatedAt,
 		UpdatedAt:   dbPost.UpdatedAt,
 		Title:       dbPost.Title,
-		Description: dbPost.Description.String,
+		Description: description,
 		PublishedAt: dbPost.PublishedAt,
 		Url:         dbPost.Url,
 		FeedID:      dbPost.FeedID,
